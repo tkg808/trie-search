c8c087a72db9b6472ab907979e27dcb86a965ae2
Tyson Gomes <tkg@TKG-MacBook-Pro.local>
2022-09-10 18:53:27 -0700

    Add basic tests

---
+++ test/test.ts
@@ -0,0 +1,28 @@
+const states = require("./data");
+const { Dictionary } = require("./src/dictionary/dictionary");
+
+const statesDictionary = new Dictionary(states);
+/* === Properties ===  */
+
+// console.log(statesDictionary.root);
+// console.log(statesDictionary.options);
+
+/* === Methods ===  */
+
+/* = Test - search =  */
+// console.log(statesDictionary.search("Ohio"));
+// console.log(statesDictionary.search("Ohi"));
+
+/* = Test - prefixSearch = */
+// console.log(statesDictionary.search("Ohi"));
+// console.log(statesDictionary.search("Ohio"));
+// console.log(statesDictionary.search("Ohis"));
+
+
+/* = Test - findMatches =  */
+// console.log(statesDictionary.findMatches("A"));
+// console.log(statesDictionary.findMatches("A", -1));
+// console.log(statesDictionary.findMatches("A", 0));
+// console.log(statesDictionary.findMatches("A", 3));
+// console.log(statesDictionary.findMatches("Al"));
+// console.log(statesDictionary.findMatches("Alabama"));
