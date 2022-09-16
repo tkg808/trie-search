29b899ded91403429da59d78a59499ef8fb8fa61
Tyson Gomes <tkg@TKG-MacBook-Pro.local>
2022-09-15 17:35:44 -0700

    Add unit tests

--- test/test.ts
+++ test/test.ts
@@ -1,28 +1,133 @@
+const Dictionary = require("../src/index");
 const states = require("./data");
-const { Dictionary } = require("./src/dictionary/dictionary");
 
-const statesDictionary = new Dictionary(states);
-/* === Properties ===  */
+let statesDictionary: typeof Dictionary;
 
-// console.log(statesDictionary.root);
-// console.log(statesDictionary.options);
+beforeEach(() =>
+{
+  statesDictionary = new Dictionary(states);
+});
 
-/* === Methods ===  */
+describe("Dictionary class", () =>
+{
+  it("root should be a private property", async () => 
+  {
+    const root = statesDictionary.root;
 
-/* = Test - search =  */
-// console.log(statesDictionary.search("Ohio"));
-// console.log(statesDictionary.search("Ohi"));
+    expect(root).toBeUndefined();
+  })
 
-/* = Test - prefixSearch = */
-// console.log(statesDictionary.search("Ohi"));
-// console.log(statesDictionary.search("Ohio"));
-// console.log(statesDictionary.search("Ohis"));
+  it("options should be a private property", async () => 
+  {
+    const options = statesDictionary.options;
 
+    expect(options).toBeUndefined();
+  })
 
-/* = Test - findMatches =  */
-// console.log(statesDictionary.findMatches("A"));
-// console.log(statesDictionary.findMatches("A", -1));
-// console.log(statesDictionary.findMatches("A", 0));
-// console.log(statesDictionary.findMatches("A", 3));
-// console.log(statesDictionary.findMatches("Al"));
-// console.log(statesDictionary.findMatches("Alabama"));
+  it("getOptions should return the values stored in options", async () => 
+  {
+    const options = statesDictionary.getOptions();
+
+    expect(options).toHaveLength(states.length);
+  })
+
+  it("options should store values in a sorted order", async () => 
+  {
+    const options: string[] = statesDictionary.getOptions();
+    const ordered: string[] = [...states].sort();
+
+    expect(options.toString()).toBe(ordered.toString());
+  })
+
+  it("insert should be a private method", async () => 
+  {
+    const result = statesDictionary.insert;
+    expect(result).toBeUndefined();
+  })
+
+  it("hasWord should return true for a word that exists in Dictionary", async () => 
+  {
+    const result = statesDictionary.hasWord("Ohio");
+
+    expect(result).toBe(true);
+  })
+
+  it("hasWord should return false for a word that doesn't exist in Dictionary", async () => 
+  {
+    const result = statesDictionary.hasWord("Ohi");
+
+    expect(result).toBe(false);
+  })
+
+  it("hasWord should return false for an empty string/input", async () => 
+  {
+    const result = statesDictionary.hasWord("");
+
+    expect(result).toBe(false);
+  })
+
+  it("hasPrefix should return true for a prefix to a word that exists in Dictionary", async () => 
+  {
+    const result = statesDictionary.hasPrefix("Ohi");
+
+    expect(result).toBe(true);
+  })
+
+  it("hasPrefix should return false for a prefix to a word that doesn't exist in Dictionary", async () => 
+  {
+    const result = statesDictionary.hasPrefix("Ohis");
+
+    expect(result).toBe(false);
+  })
+
+  it("hasPrefix should return true for an empty string/input", async () => 
+  {
+    const result = statesDictionary.hasPrefix("");
+
+    expect(result).toBe(true);
+  })
+
+  it("findMatches should return all matches that start with the given input/prefix", async () => 
+  {
+    const result = statesDictionary.findMatches("A");
+
+    expect(result.toString()).toBe(['Alabama', 'Alaska', 'Arizona', 'Arkansas'].toString());
+  })
+
+  it("findMatches should return the first X lexicographical words that start with the given input/prefix when a limit (X) is given", async () => 
+  {
+    const limitedMatches = statesDictionary.findMatches("A", 3);
+    const allMatches = statesDictionary.findMatches("A").sort();
+
+    expect(limitedMatches.toString()).toBe(allMatches.slice(0, 3).toString());
+  })
+
+  it("findMatches should return no matches when the given input/prefix doesn't exist in Dictionary", async () => 
+  {
+    const result = statesDictionary.findMatches("As");
+
+    expect(result).toHaveLength(0);
+  })
+
+  it("findMatches should return no matches when the limit given is a negative integer", async () => 
+  {
+    const result = statesDictionary.findMatches("A", -1);
+
+    expect(result).toHaveLength(0);
+  })
+
+  it("findMatches should return no matches when the limit given is 0", async () => 
+  {
+    const result = statesDictionary.findMatches("A", 0);
+
+    expect(result).toHaveLength(0);
+  })
+
+  it("findMatches should return matches that includes the input/prefix when the input/prefix completely matches a word in Dictionary", async () => 
+  {
+    const result = statesDictionary.findMatches("Alabama");
+
+    expect(result.length).toBeGreaterThanOrEqual(1);
+    expect(result.indexOf("Alaabma")).toBeTruthy();
+  })
+});
