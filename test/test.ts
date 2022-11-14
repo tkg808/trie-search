89ccdaec77592f944113e7fb8addd5643ff74b8e
Tyson Gomes <tkg@TKG-MacBook-Pro.local>
2022-11-14 15:14:54 -0800

    Update tests

--- test/test.ts
+++ test/test.ts
@@ -1,131 +1,127 @@
-const Dictionary = require("../src/index");
-const states = require("./data");
+const Trie = require("../src/index");
+const states = [
+  'Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota', 'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'
+];
 
-let statesDictionary: typeof Dictionary;
+let statesTrie: typeof Trie;
 
 beforeEach(() =>
 {
-  statesDictionary = new Dictionary(states);
+  statesTrie = new Trie(states);
 });
 
-describe("Dictionary class", () =>
+describe("Trie class", () =>
 {
   it("should not give direct access to root field", async () => 
   {
-    const root = statesDictionary.root;
+    const root = statesTrie.root;
 
     expect(root).toBeUndefined();
   })
 
-  it("should not give direct access to options field", async () => 
+  it("should not give direct access to keys field", async () => 
   {
-    const options = statesDictionary.options;
+    const keys = statesTrie.keys;
 
-    expect(options).toBeUndefined();
+    expect(keys).toBeUndefined();
   })
 
-  it("should give access to options field via getter", async () => 
+  it("should give access to keys field via getter", async () => 
   {
-    const options = statesDictionary.getOptions();
+    const keys = statesTrie.getAll();
 
-    expect(options).toHaveLength(states.length);
+    expect(keys).toHaveLength(states.length);
   })
 
-  it("should store options in a sorted order", async () => 
+  it("should store keys in a sorted order", async () => 
   {
-    const options: string[] = statesDictionary.getOptions();
+    const keys: string[] = statesTrie.getAll();
     const ordered: string[] = [...states].sort();
 
-    expect(options.toString()).toBe(ordered.toString());
+    expect(keys.toString()).toBe(ordered.toString());
   })
 
-  it("should not give access to insert method", async () => 
+  it("should return true for hasKey when the input is a key that exists in the Trie", async () => 
   {
-    const result = statesDictionary.insert;
-    expect(result).toBeUndefined();
-  })
-
-  it("should return true for hasWord when the input is a word that exists in Dictionary", async () => 
-  {
-    const result = statesDictionary.hasWord("Ohio");
+    const result = statesTrie.hasKey("Ohio");
 
     expect(result).toBe(true);
   })
 
-  it("should return false for hasWord when the input is a word doesn't exist in Dictionary", async () => 
+  it("should return false for hasKey when the input is a key that doesn't exist in the Trie", async () => 
   {
-    const result = statesDictionary.hasWord("Ohi");
+    const result = statesTrie.hasKey("Ohi");
 
     expect(result).toBe(false);
   })
 
-  it("should return false for hasWord when the input is an empty string", async () => 
+  it("should return false for hasKey when the input is an empty string", async () => 
   {
-    const result = statesDictionary.hasWord("");
+    const result = statesTrie.hasKey("");
 
     expect(result).toBe(false);
   })
 
-  it("should return true for hasPrefix when the input is a prefix to a word that exists in Dictionary", async () => 
+  it("should return true for hasPrefix when the input is a prefix to a key that exists in the Trie", async () => 
   {
-    const result = statesDictionary.hasPrefix("Ohi");
+    const result = statesTrie.hasPrefix("Ohi");
 
     expect(result).toBe(true);
   })
 
-  it("should return false for hasPrefix when the input is a prefix to a word that doesn't exist in Dictionary", async () => 
+  it("should return false for hasPrefix when the input is a prefix to a key that doesn't exist in the Trie", async () => 
   {
-    const result = statesDictionary.hasPrefix("Ohis");
+    const result = statesTrie.hasPrefix("Ohis");
 
     expect(result).toBe(false);
   })
 
-  it("should return true for hasPrefix when the input is an empty string/input", async () => 
+  it("should return true for hasPrefix when the input is an empty string", async () => 
   {
-    const result = statesDictionary.hasPrefix("");
+    const result = statesTrie.hasPrefix("");
 
     expect(result).toBe(true);
   })
 
-  it("should return all matches that start with the given input/prefix when findMatches is called", async () => 
+  it("should return all matches that start with the given input when getMatches is called", async () => 
   {
-    const result = statesDictionary.findMatches("A");
+    const result = statesTrie.getMatches("A");
 
     expect(result.toString()).toBe(['Alabama', 'Alaska', 'Arizona', 'Arkansas'].toString());
   })
 
-  it("should return the first x lexicographical matches that start with the given input/prefix when a limit (x) is provided to a findMatches call", async () => 
+  it("should return the first x lexicographical matches that start with the given input when getMatches is called with and a limit is provided", async () => 
   {
-    const limitedMatches = statesDictionary.findMatches("A", 3);
-    const allMatches = statesDictionary.findMatches("A").sort();
+    const limitedMatches = statesTrie.getMatches("A", 3);
+    const allMatches = statesTrie.getMatches("A").sort();
 
     expect(limitedMatches.toString()).toBe(allMatches.slice(0, 3).toString());
   })
 
-  it("should return no matches when findMatches is given an input that doesn't exist in Dictionary ", async () => 
+  it("should return no matches when getMatches is given an input that doesn't exist in the Trie ", async () => 
   {
-    const result = statesDictionary.findMatches("As");
+    const result = statesTrie.getMatches("As");
 
     expect(result).toHaveLength(0);
   })
 
-  it("should return no matches when findMatches is given a negative integer limit", async () => 
+  it("should return no matches when getMatches is given a negative integer limit", async () => 
   {
-    const result = statesDictionary.findMatches("A", -1);
+    const result = statesTrie.getMatches("A", -1);
 
     expect(result).toHaveLength(0);
   })
 
-  it("should return no matches when findMatches given is given a limit of 0", async () => 
+  it("should return no matches when getMatches is given a limit of 0", async () => 
   {
-    const result = statesDictionary.findMatches("A", 0);
+    const result = statesTrie.getMatches("A", 0);
 
     expect(result).toHaveLength(0);
   })
 
-  it("should return matches that includes the input if it exists in the dictionary when findMatches is called", async () => 
+  it("should return matches that includes the input when getMatches is called", async () => 
   {
-    const result = statesDictionary.findMatches("Alabama");
+    const result = statesTrie.getMatches("Alabama");
 
     expect(result.length).toBeGreaterThanOrEqual(1);
     expect(result.indexOf("Alaabma")).toBeTruthy();
